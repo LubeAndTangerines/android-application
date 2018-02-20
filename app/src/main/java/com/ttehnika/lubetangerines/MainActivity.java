fd1a588b21a59603653413709d0740441a8d114a
Markus Tarn <markus.tarn@myjar.com>
2018-02-20 12:30:21 +0200

    Disain change

--- app/src/main/java/com/ttehnika/lubetangerines/MainActivity.java
+++ app/src/main/java/com/ttehnika/lubetangerines/MainActivity.java
@@ -84,9 +84,9 @@ public class MainActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
